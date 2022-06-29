eb1056571bc179e0922c579be7145fe74b952726
Brian Svoboda <brian.e.svoboda@gmail.com>
2022-06-28 18:50:53 -0600

    Add equation citation.

--- src/escapeprob.jl
+++ src/escapeprob.jl
@@ -30,7 +30,7 @@ end
 
 Expanding sphere, Large Velocity Gradient, or Sobolev case. Formula from De
 Jong, Boland, and Dalgarno (1980, A&A 91, 68)[^DeJong80].  Corrected by a
-factor of 2 in order to return 1 for τ=0.
+factor of 2 in order to return 1 for τ=0. See Appendix B equation B-7.
 
 [^DeJong80]: [ADS abstract](https://ui.adsabs.harvard.edu/abs/1980A%26A....91...68D/abstract)
 """
